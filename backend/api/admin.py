1b5b89bff8b09e26b74e0ab6363122be572a67cb
AdityaSOfficial <aditya.s2022a@vitstudent.ac.in>
2023-04-01 19:55:47 +0530

    Models V2
    ProfileModel, PostsModel, CommentsModel has been added

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(ProfileModel)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostsModel)
+class PostsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CommentsModel)
+class CommentsAdmin(admin.ModelAdmin):
+    pass
--- backend/api/models.py
+++ backend/api/models.py
@@ -3,21 +3,33 @@ from django.db import models
 
 
 class ProfileModel(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='ID',
+                          auto_created=True, serialize=False)
     name = models.CharField(max_length=50)
-    email = models.EmailField()
-    user_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=300)
     following = models.ManyToManyField(
-        'self', null=True, blank=True)  # 'self' IMP
-    followers = models.ManyToManyField('self', null=True, blank=True)
-    following_count = models.IntegerField()
-    followers_count = models.IntegerField()
-    profile_pic = models.ImageField()  # Look this up
-    bio = models.CharField(max_length=1000)
-    date_time_created = models.DateTimeField(auto_now=True)
-    # posts = Relations - to read up on that
+        'self', blank=True, null=True)  # 'self' IMP
+    followers = models.ManyToManyField('self', blank=True, null=True)
+    profile_pic = models.ImageField(blank=True, null=True)  # Look this up
+    bio = models.CharField(max_length=1000, blank=True)
+    createdAt = models.DateTimeField(auto_now=True)
 
-    def save(self, *args, **kwargs):
-        self.following_count = len(list(self.following))
-        self.followers_coun = len(list(self.followers))
-        return super().save(self, *args, **kwargs)
+    def __str__(self):
+        return f'{self.user_name}'
+
+
+class PostsModel(models.Model):
+    user = models.ForeignKey(ProfileModel, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now=True)
+    picture = models.ImageField()
+    body = models.CharField(max_length=1000)
+    caption = models.CharField(max_length=200)
+    likes = models.IntegerField()
+
+
+class CommentsModel(models.Model):
+    post = models.ForeignKey(PostsModel, on_delete=models.CASCADE)
+    body = models.CharField(max_length=1000)
+    createdAt = models.DateTimeField(auto_now=True)
